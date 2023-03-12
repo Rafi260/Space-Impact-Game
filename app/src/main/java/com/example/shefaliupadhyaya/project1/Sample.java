f2b1922a2c7bd1e2308daa583750e5378014ac31
unknown <mdashrafrafi260@gmail.com>
2023-03-12 10:05:32 +0600

    Second

---
+++ app/src/main/java/com/example/shefaliupadhyaya/project1/Sample.java
@@ -0,0 +1,32 @@
+package com.example.shefaliupadhyaya.project1;
+
+import java.util.Random;
+
+/**
+ * Created by Shefali Upadhyaya on 13-07-2016.
+ */
+public class Sample {
+   int roll;
+   String name;
+
+    public Sample(int roll, String name) {
+        this.roll = roll;
+        this.name = name;
+    }
+
+    public int getRoll() {
+        return roll;
+    }
+
+    public void setRoll(int roll) {
+        this.roll = roll;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
